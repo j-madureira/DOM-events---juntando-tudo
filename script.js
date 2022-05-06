aa1927192cfe5fbb19f683b601a61fd873aa3bcd
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-05-06 16:08:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarP1(pInput){
+    var p1 = document.getElementById("p1");
+    p1.innerHTML = pInput.value;
+}
+
+function mudarP2(sInput){
+    var p2 = document.getElementById("p2");
+    p2.innerHTML = sInput.value;
+}
